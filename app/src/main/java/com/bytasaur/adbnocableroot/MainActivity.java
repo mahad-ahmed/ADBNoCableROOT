d66d4c1dc9d3b5732212249b218259569e9f8b39
Barksalot <herstalf@gmail.com>
2018-01-14 15:59:18 +0500

    Added menu to configure port

--- app/src/main/java/com/bytasaur/adbnocableroot/MainActivity.java
+++ app/src/main/java/com/bytasaur/adbnocableroot/MainActivity.java
@@ -3,9 +3,15 @@ package com.bytasaur.adbnocableroot;
 import android.annotation.SuppressLint;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.text.InputType;
+import android.text.TextUtils;
 import android.text.format.Formatter;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -13,13 +19,14 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
-public class MainActivity extends AppCompatActivity { // TODO: Allow configuration of port
+public class MainActivity extends AppCompatActivity {
     private Runtime runtime;
     private TextView status;
     private TextView ip_text;
     private TextView port_text;
+    private MenuItem menuPortItem;
     private WifiManager wifiManager;
-    static int PORT = 5555;
+    static int PORT;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +35,7 @@ public class MainActivity extends AppCompatActivity { // TODO: Allow configurati
 
         wifiManager = (WifiManager) getApplicationContext().getSystemService(WIFI_SERVICE);
         runtime = Runtime.getRuntime();
+        PORT = getPreferences(0).getInt("port", 5555);
         status = findViewById(R.id.state);
         ip_text = findViewById(R.id.ip);
         port_text = findViewById(R.id.port);
@@ -38,6 +46,53 @@ public class MainActivity extends AppCompatActivity { // TODO: Allow configurati
         super.onResume();
         setStatus(null);
         setIp(null);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        menuPortItem = menu.getItem(0);
+        menuPortItem.setTitle(PORT+"");
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        final EditText editText = new EditText(this);
+        editText.setInputType(InputType.TYPE_CLASS_NUMBER);
+        final AlertDialog alertDialog = new AlertDialog.Builder(this).setTitle("Enter port to be used").setView(editText)
+                .setPositiveButton("Set", null).setNeutralButton("Default", null).setNegativeButton("Cancel", null).create();
+        alertDialog.show();
+        alertDialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String str = editText.getText().toString();
+                if(TextUtils.isEmpty(str)) {
+                    editText.setError("* Required");
+                    return;
+                }
+                int port = Integer.parseInt(str);
+                if(port>0) {
+                    getPreferences(0).edit().putInt("port", port).apply();
+                    PORT = port;
+                    menuPortItem.setTitle(PORT+"");
+                    alertDialog.dismiss();
+                }
+                else {
+                    editText.setError("* Invalid port number");
+                }
+            }
+        });
+        alertDialog.getButton(AlertDialog.BUTTON_NEUTRAL).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getPreferences(0).edit().putInt("port", 5555).apply();
+                PORT = 5555;
+                menuPortItem.setTitle("5555");
+                alertDialog.dismiss();
+            }
+        });
+        return super.onOptionsItemSelected(item);
     }
 
     @SuppressLint("SetTextI18n")
